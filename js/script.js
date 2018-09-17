87228e5e2fbffcc0bc64e173f0d0a80fd375f67e
unknown <kalamar@ITI-263.iti.gr>
2018-09-17 12:09:41 +0300

    Use spreadsheet view instead of data table

--- js/script.js
+++ js/script.js
@@ -13,14 +13,68 @@ function uploadFile() {
 	document.getElementById("spinner").style.visibility = "visible";
 
 	dataLoaders[fileInfo.type](file).then(data => {		
-		rawData = data;
+		rawData = data.map(d => {
+            if (d.room)
+                return d;
+            else {
+                d.room = "no_room";
+                return d;
+            }
+        });
 		currentData = data;
-		$('#dataTable').DataTable( {
-			data: data.map(d => Object.values(d)),
-			columns: Object.keys(data[0]).map(d => ({title: d}))
-		} );		
+		// $('#dataTable').DataTable( {
+			// data: data.map(d => Object.values(d)),
+			// columns: Object.keys(data[0]).map(d => ({title: d}))
+		// } );		
 		document.getElementById("spinner").style.visibility = "hidden";
-	});
+		document.getElementById("resultsDiv").style.visibility = "visible";
+        
+        // var table = $('#dataTable').DataTable();
+        // console.log(table.rows().data());
+		
+		var container1 = document.getElementById('example1');
+		var hot1 = new Handsontable(container1, {
+			data: data,
+			height: 400,
+			colHeaders: Object.keys(data[0]),
+			rowHeaders: true,
+			filters: true,
+			dropdownMenu: true
+			// hiddenRows: { rows: [1, 3, 5], indicators: true },
+			// hiddenColumns: { columns: [1, 3, 5], indicators: true }
+		});
+		var button1 = document.getElementById('export-file-csv');
+		var button2 = document.getElementById('export-file-json');
+		var exportPlugin1 = hot1.getPlugin('exportFile');
+
+		button1.addEventListener('click', function() {
+			exportPlugin1.downloadFile('csv', {
+				bom: false,
+				columnDelimiter: ',',
+				columnHeaders: true,
+				// exportHiddenColumns: true,
+				// exportHiddenRows: true,
+				fileExtension: 'csv',
+				filename: 'data',
+				mimeType: 'text/csv',
+				rowDelimiter: '\r\n',
+				rowHeaders: false
+			});
+		});
+		
+		button2.addEventListener('click', function() {
+			var shownData = hot1.getData();
+			var headers = Object.keys(data[0]);			
+			var jsonData = shownData.map(d => {
+				var obj = {};
+				headers.forEach((h, i) => {
+					obj[h] = d[i];
+				});
+				return obj;
+			});			
+			download("data.json", JSON.stringify(jsonData));
+		});
+});
 }
 
 function download(filename, text) {
