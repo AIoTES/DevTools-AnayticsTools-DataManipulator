bb1e2d14ddad285e23d2f2146e1e6f9fcc7e2fde
unknown <kalamar@ITI-263.iti.gr>
2019-02-27 17:45:00 +0200

    Add computed columns feature

--- js/script.js
+++ js/script.js
@@ -3,44 +3,44 @@ var dataLoaders = {
 	"application/vnd.ms-excel": file => d3.csv(file)
 };
 
+hotGlobal = null;
+
+var addedColumns = {};
+
 function uploadFile() {	
 	var fileInfo = document.getElementById("fileToUpload").files[0];
 	var file = "data/" + fileInfo.name;
 	
 	document.getElementById("spinner").style.visibility = "visible";
 
-	dataLoaders[fileInfo.type](file).then(data => {		
-		rawData = data.map(d => {
-            if (d.room)
-                return d;
-            else {
-                d.room = "no_room";
-                return d;
-            }
-        });
+	dataLoaders[fileInfo.type](file).then(data => {
+
 		document.getElementById("spinner").style.visibility = "hidden";
 		document.getElementById("resultsDiv").style.visibility = "visible";
 		
-		var hot = createSpreadsheet(document.getElementById('example1'), data);
+		createSpreadsheet(document.getElementById('example1'), data);
         
-		var exportCsvBtn = document.getElementById('export-file-csv');
+        var exportCsvBtn = document.getElementById('export-file-csv');
         exportCsvBtn.addEventListener('click', function() {
-            var jsonData = tableDataToJson(hot);
+            var jsonData = tableDataToJson();
             var csvStr = jsonToCSV(jsonData);
             download("data.csv", csvStr);
-		});
+        });
         
-		var exportJsonBtn = document.getElementById('export-file-json');
-		exportJsonBtn.addEventListener('click', function() {
-			var jsonData = tableDataToJson(hot);
-			download("data.json", JSON.stringify(jsonData));
-		});
-});
+        var exportJsonBtn = document.getElementById('export-file-json');
+        exportJsonBtn.addEventListener('click', function() {
+            var jsonData = tableDataToJson();
+            download("data.json", JSON.stringify(jsonData));
+        });
+
+        addedColumns = {};
+        renderAddedColumns();
+    });
 }
 
-function tableDataToJson(hot) {
-    var data = hot.getData();
-    var headers = hot.getColHeader();			
+function tableDataToJson() {
+    var data = hotGlobal.getData();
+    var headers = hotGlobal.getColHeader();			
     var jsonData = data.map(d => {
         var obj = {};
         headers.forEach((h, i) => {
@@ -96,7 +96,7 @@ function createSpreadsheet(container, data) {
     }
     
     // create spreadsheet
-    var hot = new Handsontable(container, {
+    hotGlobal = new Handsontable(container, {
         data: data,
         height: 400,
         colHeaders: Object.keys(data[0]),
@@ -109,5 +109,76 @@ function createSpreadsheet(container, data) {
         // hiddenColumns: { columns: [1, 3, 5], indicators: true }
     });
     
-    return hot;
+    // return hot;
+}
+
+function addColumn() {
+    let colName = document.getElementById("colNameTextBox").value;
+    let expr = document.getElementById("colExprTextBox").value;
+    let data = tableDataToJson(hotGlobal);
+    let compData = addComputedAttr(data, colName, expr);
+    createSpreadsheet(document.getElementById('example1'), compData);
+
+    addedColumns[colName] = {
+        colNameTextboxID: colName + "_textbox",
+        exprTextboxID: colName + "_expr_textbox",
+        expr: expr,
+        updateBtnID: colName + "_update_btn",
+        removeBtnID: colName + "_remove_btn"
+    };
+
+    renderAddedColumns();
+}
+
+function renderAddedColumns() {
+    let container = document.getElementById("addedColumnsDiv");
+    container.innerHTML = "";
+    for (key in addedColumns) {
+        let col = addedColumns[key];
+        container.innerHTML += `
+            Name: <input type="text" value="${key}" id="${col.colNameTextboxID}" />
+            Expression: <input type="text" value="${col.expr}" id="${col.exprTextboxID}" class="exprBox" />
+            <button onclick="removeColumn('${key}')">Remove</button>
+            <br>
+        `;
+    }
+    document.getElementById("colNameTextBox").value = "";
+    document.getElementById("colExprTextBox").value = "";
+}
+
+function removeColumn(colName) {
+    let data = tableDataToJson(hotGlobal);
+    let compData = removeComputedAttr(data, colName);
+    createSpreadsheet(document.getElementById('example1'), compData);
+
+    delete addedColumns[colName];
+
+    renderAddedColumns();
+}
+
+function addComputedAttr(data, colName, expr) {
+    let res = data.map(d => {
+        let obj = {...d};
+        obj[colName] = applyExpr(expr, d);
+        return obj;
+    });
+    return res;
+}
+
+function removeComputedAttr(data, colName) {
+    let res = data.map(d => {
+        let obj = {...d};
+        delete obj[colName];
+        return obj;
+    });
+    return res;
+}
+
+function applyExpr(expr, obj) {
+    let resExpr = expr;
+    for (key in obj) {
+        let re = new RegExp('\\b' + key + '\\b');
+        resExpr = resExpr.replace(re, obj[key]);
+    }
+    return eval(resExpr);
 }
