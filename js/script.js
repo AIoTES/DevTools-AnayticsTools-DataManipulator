3ef8904f7da1a404dd8056291174fe89f589e0a5
unknown <kalamar@ITI-263.iti.gr>
2019-02-27 12:03:05 +0200

    Organize code better

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-var rawData;
-var currentData;
-
 var dataLoaders = {
 	"application/json": file => d3.json(file),
 	"application/vnd.ms-excel": file => d3.csv(file)
@@ -21,75 +18,96 @@ function uploadFile() {
                 return d;
             }
         });
-		currentData = data;
-		// $('#dataTable').DataTable( {
-			// data: data.map(d => Object.values(d)),
-			// columns: Object.keys(data[0]).map(d => ({title: d}))
-		// } );		
 		document.getElementById("spinner").style.visibility = "hidden";
 		document.getElementById("resultsDiv").style.visibility = "visible";
+		
+		var hot = createSpreadsheet(document.getElementById('example1'), data);
         
-        // var table = $('#dataTable').DataTable();
-        // console.log(table.rows().data());
-		
-		var container1 = document.getElementById('example1');
-		var hot1 = new Handsontable(container1, {
-			data: data,
-			height: 400,
-			colHeaders: Object.keys(data[0]),
-			rowHeaders: true,
-			filters: true,
-			dropdownMenu: true
-			// hiddenRows: { rows: [1, 3, 5], indicators: true },
-			// hiddenColumns: { columns: [1, 3, 5], indicators: true }
+		var exportCsvBtn = document.getElementById('export-file-csv');
+        exportCsvBtn.addEventListener('click', function() {
+            var jsonData = tableDataToJson(hot);
+            var csvStr = jsonToCSV(jsonData);
+            download("data.csv", csvStr);
 		});
-		var button1 = document.getElementById('export-file-csv');
-		var button2 = document.getElementById('export-file-json');
-		var exportPlugin1 = hot1.getPlugin('exportFile');
-
-		button1.addEventListener('click', function() {
-			exportPlugin1.downloadFile('csv', {
-				bom: false,
-				columnDelimiter: ',',
-				columnHeaders: true,
-				// exportHiddenColumns: true,
-				// exportHiddenRows: true,
-				fileExtension: 'csv',
-				filename: 'data',
-				mimeType: 'text/csv',
-				rowDelimiter: '\r\n',
-				rowHeaders: false
-			});
-		});
-		
-		button2.addEventListener('click', function() {
-			var shownData = hot1.getData();
-			var headers = Object.keys(data[0]);			
-			var jsonData = shownData.map(d => {
-				var obj = {};
-				headers.forEach((h, i) => {
-					obj[h] = d[i];
-				});
-				return obj;
-			});			
+        
+		var exportJsonBtn = document.getElementById('export-file-json');
+		exportJsonBtn.addEventListener('click', function() {
+			var jsonData = tableDataToJson(hot);
 			download("data.json", JSON.stringify(jsonData));
 		});
 });
 }
 
+function tableDataToJson(hot) {
+    var data = hot.getData();
+    var headers = hot.getColHeader();			
+    var jsonData = data.map(d => {
+        var obj = {};
+        headers.forEach((h, i) => {
+            obj[h] = d[i];
+        });
+        return obj;
+    });
+    return jsonData;
+}
+
+function jsonToCSV(data) {
+    var headers = Object.keys(data[0]);
+    var csvStr = "";
+    headers.forEach((header, i) => {
+        if (i > 0) {
+            csvStr += ",";
+        }
+        csvStr += header;
+    });
+    csvStr += "\n";
+    data.forEach(record => {
+        headers.forEach((header, i) => {
+            if (i > 0) {
+                csvStr += ",";
+            }
+            csvStr += record[header];
+        });
+        csvStr += "\n";
+    });
+    return csvStr;
+}
+
 function download(filename, text) {
-  var element = document.createElement('a');
-  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
-  element.setAttribute('download', filename);
-
-  element.style.display = 'none';
-  document.body.appendChild(element);
-
-  element.click();
-
-  document.body.removeChild(element);
+	var element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
 }
 
 function exportResults() {
 	download("results.json", JSON.stringify(features));
+}
+
+function createSpreadsheet(container, data) {
+    // clear previous contents
+    container.innerHTML = "";
+    var licenceDiv = document.getElementById("hot-display-license-info");
+    if (licenceDiv) {
+        licenceDiv.remove();
+    }
+    
+    // create spreadsheet
+    var hot = new Handsontable(container, {
+        data: data,
+        height: 400,
+        colHeaders: Object.keys(data[0]),
+        rowHeaders: true,
+        filters: true,
+        dropdownMenu: true,
+        columnSorting: true,
+        contextMenu: true
+        // hiddenRows: { rows: [1, 3, 5], indicators: true },
+        // hiddenColumns: { columns: [1, 3, 5], indicators: true }
+    });
+    
+    return hot;
 }
